8107af498a7285512c5a9541074032f45347833a
Bryan Stitt <bryan@stitthappens.com>
2020-07-26 14:03:33 -0700

    add more to prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -4,4 +4,7 @@ pub use crate::{Error, Sx127x};
 
 pub use crate::device::fsk::{FskChannel, FskConfig};
 pub use crate::device::lora::{LoRaChannel, LoRaConfig};
-pub use crate::device::{Channel, Config, Modem, PacketInfo};
+pub use crate::device::{Channel, Config, Modem, PacketInfo, State};
+
+pub use radio::{Receive as ReceiveTrait, State as StateTrait, Transmit as TransmitTrait};
+pub use embedded_spi::wrapper::Wrapper as SpiWrapper;
